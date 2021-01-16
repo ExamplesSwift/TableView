71b31ed57f4df3338ce0094d3531bd400572a74e
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2021-01-16 12:07:35 -0300

    - Create strcut class for cell - Refactor the ViewController to LibraryViewController - Config class for our table

---
+++ TableViews/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  TableViews
+//
+//  Created by Henry AT on 16-01-21.
+//
+
+import UIKit
+
+struct Book {
+  let title: String
+  let author: String
+  let image: UIImage
+}
---
+++ TableViews/LibraryViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Henry AT on 16-01-21.
+//
+
+import UIKit
+
+class LibraryViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    55
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    <#code#>
+  }
+
+}
+
